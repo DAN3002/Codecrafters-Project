aa94af35d72c4c0c857aeb75dcfcb759d9eff957
DAN3002 <dinhanh300229@gmail.com>
2024-10-01 22:51:24 +0700

    feat: add sync.sh

---
+++ sync.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/zsh
+
+# Passing the project name as an argument
+project_name=$1
+
+# Syncing the project
+echo "Syncing project: $project_name"
+source_path="../codecrafters-$project_name-python"
+target_path="./$project_name-python"
+
+# Makedir if not exists
+if [ ! -d $target_path ]; then
+	mkdir $target_path
+fi
+cp -rf $source_path/* $target_path/
+cd $target_path
+rm -rf .git
+
+# Committing the changes
+git add .
+echo "Done, please commit to git"
