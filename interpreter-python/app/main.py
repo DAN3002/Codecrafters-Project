3eba51299abad5a89b46d12b929cd68c90fd6bd6
DAN3002 <dinhanh300229@gmail.com>
2024-10-04 16:16:37 +0700

    feat(interpreter): Scanning: Assignment & equality Operators

--- interpreter-python/app/main.py
+++ interpreter-python/app/main.py
@@ -1,10 +1,8 @@
 import sys
 
-
 def exit_program(code):
 	print("EOF  null")
 	exit(code)
-
 
 def main():
 	if len(sys.argv) < 3:
@@ -23,7 +21,9 @@ def main():
 	with open(filename) as file:
 		file_contents = file.read()
 
-		for c in file_contents:
+		index = 0
+		while index < len(file_contents):
+			c = file_contents[index]
 			if c == "(":
 				print("LEFT_PAREN ( null")
 			elif c == ")":
@@ -48,13 +48,29 @@ def main():
 				print("SLASH / null")
 			elif c == "\n":
 				line_num += 1
+			elif c == "=":
+				if check_next_char(file_contents, index + 1, "="):
+					print("EQUAL_EQUAL == null")
+					index += 1
+				else:
+					print("EQUAL = null")
 			else:
 				print(f"[line {line_num}] Error: Unexpected character: {
 					  c}", file=sys.stderr)
 				have_err_scan = True
+    
+			index += 1
 
 	exit_program(0 if not have_err_scan else 65)
 
+def check_next_char(file_contents, index, expected_char):
+	if index >= len(file_contents):
+		return False
+
+	if file_contents[index] != expected_char:
+		return False
+
+	return True
 
 if __name__ == "__main__":
 	main()
