1e8402586e34ac447250a62e0a7f9c5a01cdca5d
DAN3002 <dinhanh300229@gmail.com>
2024-10-08 14:20:29 +0700

    feat(bittorrent): Decode bencoded integers

--- bittorrent-python/app/main.py
+++ bittorrent-python/app/main.py
@@ -14,6 +14,11 @@ def decode_bencode(bencoded_value):
 		if first_colon_index == -1:
 			raise ValueError("Invalid encoded value")
 		return bencoded_value[first_colon_index+1:]
+	elif chr(bencoded_value[0]) == 'i':
+		end_int_index = bencoded_value.find(b"e")
+		if end_int_index == -1:
+			raise ValueError("Invalid encoded value")
+		return int(bencoded_value[1:end_int_index])
 	else:
 		raise NotImplementedError("Only strings are supported at the moment")
 
