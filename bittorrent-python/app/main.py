d9c61dbd1e19f1475763a50710ff96b34d15597b
DAN3002 <dinhanh300229@gmail.com>
2024-10-09 18:03:44 +0700

    feat(bittorrent): Peer handshake

--- bittorrent-python/app/main.py
+++ bittorrent-python/app/main.py
@@ -1,10 +1,13 @@
 import json
 import sys
 import requests
+import socket
 
 from app.utils import bytes_to_str
 from app.decoder import decode_bencode
 from app.torrent_file import read_info
+
+PEER_ID = "00112233445566778899"
 
 def main():
 	command = sys.argv[1]
@@ -33,7 +36,7 @@ def main():
 
 		request_parrams = {
 			'info_hash': torrent_file_info['info_hash'].digest(),
-			'peer_id': "00112233445566778899",
+			'peer_id': PEER_ID,
 			'port': 6881,
 			'uploaded': 0,
 			'downloaded': 0,
@@ -49,6 +52,31 @@ def main():
 			ip = '.'.join([str(b) for b in peer[:4]])
 			port = int.from_bytes(peer[4:])
 			print(f"{ip}:{port}")
+	elif command == 'handshake':
+		filepath = sys.argv[2]
+		url = sys.argv[3]
+
+		# Spit ip and port
+		(ip, port) = url.split(":")
+
+		# Creating a socket object for the server
+		client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		client.connect((ip, int(port)))
+
+		# Create handshake message
+		torrent_file_info = read_info(filepath)
+		info_hash = torrent_file_info['info_hash']
+		handshake_msg = (
+			b"\x13BitTorrent protocol\x00\x00\x00\x00\x00\x00\x00\x00"
+			+ info_hash.digest()
+			+ PEER_ID.encode()
+		)
+
+		# Send handshake
+		client.send(handshake_msg)
+		peer_id = client.recv(68)[48:].hex()
+		print(f"Peer ID: {peer_id}")
+
 	else:
 		raise NotImplementedError(f"Unknown command {command}")
 
